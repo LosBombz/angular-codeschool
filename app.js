bb44260e4652b22fdf168b4faa742860c7839139
Carlos Escobar <carlos.escobar@logmein.com>
2016-04-08 00:20:44 -0400

    prgress

---
+++ app.js
@@ -0,0 +1,85 @@
+(function(){
+    var app = angular.module('store', []);
+
+    var gems = [
+        {
+        name: 'Dodecahedron',
+        price: 2.95,
+        description: 'Bling BLing',
+        canPurchase: true,
+        soldOut: true,
+        images: [
+            {
+                full: 'http://placehold.it/150x150',
+                thumb: 'http://placehold.it/50x50'
+            },
+            {
+                full: 'http://placehold.it/150x150',
+                thumb: 'http://placehold.it/50x50'
+            },
+            {
+                full: 'http://placehold.it/150x150',
+                thumb: 'http://placehold.it/50x50'
+            }
+        ],
+        reviews: [
+            {
+                stars: 5,
+                body: 'I love this product',
+                author: 'gemdude@email.com'
+            }
+        ]
+        },
+        {
+        name: 'Pentagonal Gem',
+        price: 5.95,
+        description: 'it\'s like a gem bro',
+        canPurchase: false,
+        images: [
+            {
+                full: 'http://placehold.it/150x150',
+                thumb: 'http://placehold.it/50x50'
+            },
+            {
+                full: 'http://placehold.it/150x150',
+                thumb: 'http://placehold.it/50x50'
+            },
+            {
+                full: 'http://placehold.it/150x150',
+                thumb: 'http://placehold.it/50x50'
+            }
+        ],
+        reviews: []
+        }
+    ];
+
+    app.controller('StoreController', function(){
+        this.products = gems;
+    });
+
+    app.controller('PanelController', function(){
+        this.tab = 1;
+
+        this.selectTab = function(tab){
+            this.tab = tab;
+        };
+
+        this.isSelected = function(tab){
+            return this.tab === tab;
+        };
+
+    });
+
+    app.controller('ReviewController', function(){
+        this.review = {};
+
+        this.addReview = function(product){
+            product.reviews.push(this.review);
+            this.review = {};
+        };
+
+    });
+
+}());
+
+

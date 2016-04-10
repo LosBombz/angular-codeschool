08c79cdd4a33b466686251a681f53a30bcda087d
Carlos Escobar <carlos.escobar@logmein.com>
2016-04-10 13:48:56 -0400

    adding more directive love

--- app.js
+++ app.js
@@ -1,85 +1,84 @@
-(function(){
+(function() {
     var app = angular.module('store', []);
 
-    var gems = [
-        {
+    var gems = [{
         name: 'Dodecahedron',
         price: 2.95,
         description: 'Bling BLing',
         canPurchase: true,
         soldOut: true,
-        images: [
-            {
-                full: 'http://placehold.it/150x150',
-                thumb: 'http://placehold.it/50x50'
-            },
-            {
-                full: 'http://placehold.it/150x150',
-                thumb: 'http://placehold.it/50x50'
-            },
-            {
-                full: 'http://placehold.it/150x150',
-                thumb: 'http://placehold.it/50x50'
-            }
-        ],
-        reviews: [
-            {
-                stars: 5,
-                body: 'I love this product',
-                author: 'gemdude@email.com'
-            }
-        ]
-        },
-        {
+        images: [{
+            full: 'http://placehold.it/150x150',
+            thumb: 'http://placehold.it/50x50'
+        }, {
+            full: 'http://placehold.it/150x150',
+            thumb: 'http://placehold.it/50x50'
+        }, {
+            full: 'http://placehold.it/150x150',
+            thumb: 'http://placehold.it/50x50'
+        }],
+        reviews: [{
+            stars: 5,
+            body: 'I love this product',
+            author: 'gemdude@email.com'
+        }]
+    }, {
         name: 'Pentagonal Gem',
         price: 5.95,
         description: 'it\'s like a gem bro',
         canPurchase: false,
-        images: [
-            {
-                full: 'http://placehold.it/150x150',
-                thumb: 'http://placehold.it/50x50'
-            },
-            {
-                full: 'http://placehold.it/150x150',
-                thumb: 'http://placehold.it/50x50'
-            },
-            {
-                full: 'http://placehold.it/150x150',
-                thumb: 'http://placehold.it/50x50'
-            }
-        ],
+        images: [{
+            full: 'http://placehold.it/150x150',
+            thumb: 'http://placehold.it/50x50'
+        }, {
+            full: 'http://placehold.it/150x150',
+            thumb: 'http://placehold.it/50x50'
+        }, {
+            full: 'http://placehold.it/150x150',
+            thumb: 'http://placehold.it/50x50'
+        }],
         reviews: []
-        }
-    ];
+    }];
 
-    app.controller('StoreController', function(){
+    app.controller('StoreController', function() {
         this.products = gems;
     });
 
-    app.controller('PanelController', function(){
-        this.tab = 1;
-
-        this.selectTab = function(tab){
-            this.tab = tab;
-        };
-
-        this.isSelected = function(tab){
-            return this.tab === tab;
-        };
-
-    });
-
-    app.controller('ReviewController', function(){
+    app.controller('ReviewController', function() {
         this.review = {};
 
-        this.addReview = function(product){
+        this.addReview = function(product) {
             product.reviews.push(this.review);
             this.review = {};
         };
 
     });
 
+    app.directive('productTitle', function() {
+        return {
+            restrict: 'E',
+            templateUrl: 'product-title.html'
+        };
+    });
+
+    app.directive('productPanels', function() {
+        return {
+            restrict: "E",
+            templateUrl: "product-panels.html",
+            controller: function() {
+                this.tab = 1;
+
+                this.selectTab = function(tab) {
+                    this.tab = tab;
+                };
+
+                this.isSelected = function(tab) {
+                    return this.tab === tab;
+                };
+
+            },
+            controllerAs: 'panel'
+        };
+    });
+
 }());
-
-

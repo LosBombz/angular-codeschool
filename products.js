6c615634355551d388c45cd953d68490b29e3890
Carlos Escobar <carlos.escobar@logmein.com>
2016-04-11 08:27:49 -0400

    school is out

---
+++ products.js
@@ -0,0 +1,35 @@
+(function() {
+    var app = angular.module('store-products', []);
+    app.directive('productTitle', function() {
+        return {
+            restrict: 'E',
+            templateUrl: 'product-title.html'
+        };
+    });
+
+    app.directive('productPanels', function() {
+        return {
+            restrict: "E",
+            templateUrl: "product-panels.html",
+            controller: function() {
+                this.tab = 1;
+
+                this.selectTab = function(tab) {
+                    this.tab = tab;
+                };
+
+                this.isSelected = function(tab) {
+                    return this.tab === tab;
+                };
+
+            },
+            controllerAs: 'panel'
+        };
+    });
+
+    app.directive('productGallery', function() {
+        return {
+            restrict: 'E'
+        };
+    });
+}());
